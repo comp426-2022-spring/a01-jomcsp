bf6c284fac5bceeebdd2543e686fd741a59bbd04
jomcsp <jomcsp@live.unc.edu>
2022-01-27 15:51:59 -0500

    changed port mechanic

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ var fs = require('fs');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2));
 // Define allowed argument name 'port'.
-args['port'];
+args["port"];
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || process.env.PORT || 3000;
+const port = args.port;
+if(!port) {
+  port = 3000;
+}
+console.log(port);
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
@@ -20,6 +25,7 @@ fs.readFile('./www/index.html/' ,'utf8', (err, data) => {
 if (err) {
     console.error(err)
     return
+    process.exit(1)
   }
   const server = http.createServer((req, res) => {
     res.statusCode = 200
